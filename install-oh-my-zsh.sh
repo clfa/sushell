16b6ca1c2e5a24b7ce6efacc05496753292fbf0c
liangfengchen <liangfengchen@yahoo.com>
2015-05-18 06:48:32 +0800

    Update install-oh-my-zsh.sh

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -7,6 +7,7 @@
 
 if [ ! which "git" > /dev/null 2>&1 ] && [ ! which "wget" > /dev/null 2>&1 ] && [ ! which "zsh" > /dev/null 2>&1 ]; then
     echo "To install oh-my-zsh, you must installed zsh, wget and git at first."
-    return
+    exit 1
 fi
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+exit 0
