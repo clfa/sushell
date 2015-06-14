1d0fcf6b56ea3033e073df896d556be5eac5bc2f
clf <clf@clf.cn>
2015-06-14 09:21:02 +0800

    add batdown update method

---
+++ batdown.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Read from files list and git clone my codes.
+if [ -e repo.list ]; then
+    fl=`cat repo.list`
+else
+    echo "Can not find repo.list file."
+    exit 1
+fi
+
+logf="batdown.log"
+test -e $logf && rm $logf
+for mf in $fl
+do
+    if [ "$1"x = "-u"x ]; then
+        cd ${mf} && /usr/bin/git pull && cd ..
+        if [ "$?"x = "0"x ]; then
+            echo "Update ${mf} successed." >> $logf
+        else
+            echo "Update ${mf} failed." >> $logf
+        fi
+    else
+        /usr/bin/git clone "https://github.com/liangfengchen/${mf}.git"
+        test "$?"x = "0"x && echo "git clone ${mf} successed." >> $logf
+    fi
+done
+exit 0
